2d96aa447c2d7f58fe8c2fd63f8f85f3472dbd90
BogdanEraser <erasermail2008@gmail.com>
2015-10-06 23:33:32 +0300

    from netbook

--- src/main/java/WorkWithXML/XMLValidator.java
+++ src/main/java/WorkWithXML/XMLValidator.java
@@ -1,11 +1,11 @@
 package WorkWithXML;
 
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -13,31 +13,17 @@ import java.io.IOException;
  */
 public class XMLValidator {
 
-    public boolean isValid() {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setValidating(true);
-        DocumentBuilder builder = null;
+    public static boolean validateXMLByXSD(File xml, File xsd) throws SAXException, IOException {
         try {
-            builder = factory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
+            SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+                    .newSchema(xsd)
+                    .newValidator()
+                    .validate(new StreamSource(xml));
+        } catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
-        Document document = null;
-
-//        SchemaFactory factory =
-//                SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-//        Schema schema = factory.newSchema(new StreamSource(pathXsd));
-//        Validator validator = schema.newValidator();
-//        validator.validate(new StreamSource(pathXml));
-
-        try {
-            document = builder.parse("plcs.xml");
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if document.
         return true;
     }
+}
 }
