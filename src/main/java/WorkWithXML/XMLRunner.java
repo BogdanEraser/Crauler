afdd9cce71c6ff0e236b2c947fc94ec25e35a5c2
BogdanEraser <erasermail2008@gmail.com>
2015-10-08 16:27:16 +0300

    from 143-

---
+++ src/main/java/WorkWithXML/XMLRunner.java
@@ -0,0 +1,23 @@
+package WorkWithXML;
+
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Bogdan Kukharskiy on 08.10.15.
+ */
+public class XMLRunner {
+    public static void main(String[] args) {
+        File fileXML = new File("D:\\Java\\com\\SourceIT\\Crauler\\src\\main\\java\\WorkWithXML\\plcs.xml");
+        File fileXSD = new File("D:\\Java\\com\\SourceIT\\Crauler\\src\\main\\java\\WorkWithXML\\plcs_scheme.xsd");
+        try {
+            System.out.println(XMLValidator.validateXMLByXSD(fileXML, fileXSD));
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
